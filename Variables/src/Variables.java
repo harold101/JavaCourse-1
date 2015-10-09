81872323425e4f4aa17ea0f49bbc8f829684b013
unknown <20018417@H03-49-03-HPE.glasgowclyde.ac.uk>
2015-10-09 15:43:10 +0100

    added some files

---
+++ Variables/src/Variables.java
@@ -0,0 +1,34 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numberOfStudents;
+		
+		String name;
+		
+		double balance;
+		
+		boolean isFinished;
+		
+		char firstInital, lastInital;
+		name="ross";
+		balance=102.50;
+		numberOfStudents=21;
+		isFinished=true;
+		firstInital='r';
+		lastInital='g';
+		System.out.println(name + "  " + firstInital + " " + balance );
+		double rate, interest, principal;
+		rate=0.7;
+		principal=300;
+		interest = rate * principal;
+		System.out.print(interest);
+		
+		
+		
+		
+	}
+
+}
